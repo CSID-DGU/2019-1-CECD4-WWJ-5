d59dea2df63df5a5cf6c6c424855acc84204568f
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-07-01 11:17:46 +0900

    Create app.js

---
+++ wwj_web/app.js
@@ -0,0 +1,20 @@
+var express = require('express'); //웹서버 사용
+var expressLayouts = require('express-ejs-layouts')
+var bodyParser = require('body-parser');
+var app = express();
+
+
+
+app.locals.pretty = true; // html code readability
+app.set('views', './views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(3303, function(){
+  console.log('Server Start...');
+})

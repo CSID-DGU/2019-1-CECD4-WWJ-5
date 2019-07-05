67f14bf029831970c28e9ec2a9282d6165c15884
PMinJoo <pmj731nm@gmail.com>
2019-07-05 16:29:00 +0900

    로그인 백엔드

--- wwj_web/app.js
+++ wwj_web/app.js
@@ -5,6 +5,24 @@ var app = express();
 
 var mysql = require('mysql')
 
+//mysql 커넥션 생성
+var connection = mysql.createConnection({
+  host: "localhost", //서버 로컬 IP
+  user: "root", //계정 아이디
+  password: "1234", //계정 비밀번호
+  database: "wwj" //접속할 DB
+})
+
+// ------- Create Session -------
+var createSession = function createSession() {
+  return function (req, res, next) {
+    if (!req.session.login) {
+      req.session.login = 'logout';
+    }
+    next();
+  };
+};
+
 app.locals.pretty = true; // html code readability
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -12,6 +30,14 @@ app.use(express.static('public'));
 //app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+  secret: '1234DSFs@adf1234!@#$asd',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 600000 },
+}));
+app.use(createSession());
+
 var router = require('./routes/index.js')(app);
 app.use('/', router);
 
--- wwj_web/routes/index.js
+++ wwj_web/routes/index.js
@@ -1,14 +1,86 @@
 module.exports = function (app) {
   var express = require('express');
   var router = express.Router();
+  var mysql = require('mysql');
   var JSAlert = require("js-alert");
   var url = require('url');
-  var dateFormat = require('dateformat');
-  var crypto = require('crypto');
 
-  router.get('/', function(req, res){
+  var connection = mysql.createConnection({
+		host: "localhost", //서버 로컬 IP
+		user: "root", //계정 아이디
+		password: "1234", //계정 비밀번호
+		database: "wwj" //접속할 DB
+	})
+
+  router.get('/', function (req, res) {
+		res.status(200);
+		res.render('index', {
+			url: req.url,
+			login: req.session.login,
+			userid: req.session.userID,
+      username: req.session.username
+		});
+	})
+
+  //login
+  router.post('/login', function(req, res) {
+    var id = req.body.userID;
+    var pw = req.body.userPW;
     res.status(200);
-    res.render('index');
+    var queryString = 'select * from user where userid=? and userpw=?'
+    connection.query(queryString, [id, pw], function(error, data){
+      if(error){
+        console.log(error);
+        res.redirect('/');
+      } else if (!data[0]){
+        res.send({msg: "아이디 또는 비밀번호를 확인해주세요!"});
+      } else{
+        var user = data[0];
+        req.session.userID = id;
+        req.session.username = user.name;
+        req.session.login = 'login';
+        res.redirect('/');
+        console.log(req.session.login);
+      }
+    });
+  })
+
+  //Logout
+  router.get('/logout', function(req, res){
+    req.session.login = 'logout';
+    res.status(200);
+    res.redirect('/');
+  })
+
+  //signup
+  router.get('/join', function(req, res){
+    res.status(200);
+    res.render('join', {
+      url: req.url,
+      login: req.session.login,
+      username: req.session.username
+    });
+  })
+
+  //signup Post
+  router.post('/join', function(req, res){
+    if(req.body.new_userPW == req.body.new_userPW_confirm){
+      req.new_userID = req.body.new_userID;
+      req.new_userPW = req.body.new_userPW;
+      req.new_userName = req.body.new_userName;
+
+      var queryString = 'INSERT INTO user (userid, name, password, salt) VALUES(?, ?, ?, ?)';
+      var params = [req.body.new_userID, req.body.new_userName, req.body.new_userPW, '1234'];
+      connection.query(queryString, params, function(err, rows) {
+        if(err){
+          console.log(err);
+        }
+      });
+      res.redirect('/');
+    } else{
+      res.redirect('/');
+    };
+    res.status(200);
   })
 
   router.get('/about', function(req, res){

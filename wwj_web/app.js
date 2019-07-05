4fbda4f14b518c280ef69c25da935177d98c368c
ozyeon <0609ojy@naver.com>
2019-07-05 15:29:24 +0900

    레이아웃 설정2

--- wwj_web/app.js
+++ wwj_web/app.js
@@ -1,5 +1,6 @@
 var express = require('express'); //웹서버 사용
 var session = require('express-session')
+, expressLayouts = require('express-ejs-layouts')
 var bodyParser = require('body-parser');
 var app = express();
 

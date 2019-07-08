00e34c7b5c3dd65815e2ca8c699c287edfe1884f
PMinJoo <pmj731nm@gmail.com>
2019-07-08 11:39:20 +0900

    트위터 크롤러 웹에 연결 완료

--- wwj_web/routes/index.js
+++ wwj_web/routes/index.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
     //pythonPath : '/usr/bin/python3',
     pythonPath : '',
     pythonOptions : ['-u'],
-    scriptPath : '/home/wwj/2019-1-CECD4-WWJ-5/wwj_web',
+    scriptPath : '/home/wwj/2019-1-CECD4-WWJ-5/crawler/tweeter',
     //scriptPath : '',
     args : ['value1', 'value2', 'value3', 'value4', 'value5', 'value6', 'value7', 'value8', 'value9', 'value10', 'value11', 'value12', 'value13', 'value14', 'value15', 'value16', 'value17', 'value18', 'value19', 'value20']
   };

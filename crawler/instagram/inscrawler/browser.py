fcc553fe7802fa9181a85b6c5b181648b3e22686
PMinJoo <pmj731nm@gmail.com>
2019-07-10 16:10:29 +0900

    Update browser.py

--- crawler/instagram/inscrawler/browser.py
+++ crawler/instagram/inscrawler/browser.py
@@ -14,11 +14,13 @@ class Browser:
         dir_path = os.path.dirname(os.path.realpath(__file__))
         service_args = ["--ignore-ssl-errors=true"]
         chrome_options = Options()
-        if not has_screen:
-            chrome_options.add_argument("--headless")
+        # if not has_screen:
+        #     chrome_options.add_argument("--headless")
         # chrome_options.headless = True
-        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Chrome(
             # executable_path="%s/bin/chromedriver" % dir_path,
             service_args=service_args,

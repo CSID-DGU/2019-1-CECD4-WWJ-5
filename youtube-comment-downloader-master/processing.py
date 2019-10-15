115522d81e673e14959ec2e20a33b81c55b834ac
ozyeon <0609ojy@naver.com>
2019-10-15 22:43:33 +0900

    수우저엉

--- youtube-comment-downloader-master/processing.py
+++ youtube-comment-downloader-master/processing.py
@@ -1,5 +1,5 @@
-with open("final.txt", "w", encoding="utf8") as fi:
-    with open("test.txt", 'r', encoding="utf8") as f:
+with open("final.txt", "w") as fi:
+    with open("test.txt", 'r') as f:
         line_num = 1
         line_data = f.readline()
         while line_data:

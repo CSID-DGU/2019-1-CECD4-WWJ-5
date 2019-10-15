3f035705d780f72c0e3438b5bda22bbf0020e5cc
ozyeon <0609ojy@naver.com>
2019-10-15 22:14:51 +0900

    melon 음악 설명 크롤링 파일 하나로 통합

--- crawler/melon/meloncomments.py
+++ crawler/melon/meloncomments.py
@@ -23,3 +23,45 @@ print(tags)
 file = open('result1.txt', 'w')
 file.write(str(tags))
 file.close()
+
+with open("result2.txt", "w") as fi:
+    with open("result1.txt", 'r') as f:
+        line_num = 1
+        line_data = f.readline()
+        while line_data:
+            start = line_data.find('&lt;strong&gt;')
+            end = line_data.find('&lt;/div&gt;",')
+            fi.write(line_data[start:end])
+            line_data = f.readline()
+            line_num += 1
+
+
+# 입,출력 파일명
+INPUT_FILE_NAME = 'result2.txt'
+OUTPUT_FILE_NAME = 'melonresult.txt'
+
+def clean_text(text):
+    cleaned_text = re.sub('&lt;/strong&gt;' , '', text)
+    cleaned_text = re.sub('&lt;/div&gt;','', cleaned_text)
+    cleaned_text = re.sub('&amp;lsquo;','', cleaned_text)
+    cleaned_text = re.sub('&lt;strong&gt;','', cleaned_text)
+    cleaned_text = re.sub('&lt;div&gt;	','', cleaned_text)
+    cleaned_text = re.sub('&amp;nbsp;','', cleaned_text)
+    cleaned_text = re.sub('&amp;rsquo; ','', cleaned_text)
+    cleaned_text = re.sub('&amp;rsquo;','', cleaned_text)
+    cleaned_text = re.sub('&amp;#39;','', cleaned_text)
+    cleaned_text = re.sub('&lt;br /&gt;	','', cleaned_text)
+    return cleaned_text
+
+# 메인 함수
+def main():
+    read_file = open(INPUT_FILE_NAME, 'r')
+    write_file = open(OUTPUT_FILE_NAME, 'w')
+    text = read_file.read()
+    text = clean_text(text)
+    write_file.write(text)
+    read_file.close()
+    write_file.close()
+
+if __name__ == "__main__":
+    main()

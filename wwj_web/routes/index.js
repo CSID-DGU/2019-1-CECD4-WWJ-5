379016171bc05f2aa6fdb6016820b9eae0ef0a52
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-11-22 20:53:13 +0900

    Update index.js

--- wwj_web/routes/index.js
+++ wwj_web/routes/index.js
@@ -325,9 +325,6 @@ module.exports = function (app) {
               m_fst_val = 0.0000;
               m_snd_val = 0.0000;
               m_trd_val = 0.0000;
-              m_max_emotion = 'none';
-              m_max_val = 0.0000;
-              m_max_mno = 0;
 
               if(rows2[i].ant > 0){
                 if(m_fst_val<rows2[i].ant){
@@ -485,10 +482,11 @@ module.exports = function (app) {
               }
               m_list.push({m1_val:m_fst_val, m2_val:m_snd_val, m3_val:m_trd_val, m1_emo:m_fst_emotion, m2_emo:m_snd_emotion, m3_emo:m_trd_emotion, m_no:recom_mno});
               console.log(m_fst_emotion);
-              if(m_fst_emotion==fst_emotion && m_max_val<m_fst_val){
+              if(m_fst_emotion==fst_emotion){
                 m_max_val = m_fst_val;
                 m_fst_emotion = fst_emotion;
                 m_max_mno = recom_mno;
+                console.log("match!!");
               }
               console.log(m_max_emotion);
             } //for

9b4732ee064b385cc5c90800182456f1aef3085d
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-11-22 20:07:52 +0900

    test

--- wwj_web/app.js
+++ wwj_web/app.js
@@ -10,8 +10,8 @@ var connection = mysql.createConnection({
   host: "localhost", //서버 로컬 IP
   user: "root", //계정 아이디
   password: "1234", //계정 비밀번호
-  database: "wwj", //접속할 DB
-  multipleStatements: true
+  database: "wwj" //접속할 DB
+  //multipleStatements: true
 })
 
 // ------- Create Session -------

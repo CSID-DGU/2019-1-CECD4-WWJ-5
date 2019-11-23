8ad34b21c791517ac028c247e480e1705cd83e0d
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-11-23 11:16:42 +0900

    Update index.js

--- wwj_web/routes/index.js
+++ wwj_web/routes/index.js
@@ -507,7 +507,6 @@ module.exports = function (app) {
                   username: req.session.username,
                   user_sentiment1: fst_emotion, user_sentiment2: snd_emotion,
                   ant_val: ant_emotion_value, joy_val: joy_emotion_value, tru_val: tru_emotion_value, fea_val: fea_emotion_value, sur_val: sur_emotion_value, sad_val: sad_emotion_value, dis_val: dis_emotion_value, ang_val: ang_emotion_value,
-                  user_interest1: keyword1, user_interest2:keyword2, user_interest3:keyword3,
                   recom_mno: m_max_mno,recom_1: m_max_emotion,
                   recom_title: recom_title,
                   recom_artist: recom_artist,

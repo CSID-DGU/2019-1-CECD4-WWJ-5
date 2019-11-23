ea10ab27f02d8e2245751b3fb884fe55267f0289
DESKTOP-N9U7MBP\PMinJoo <pmj731nm@gmail.com>
2019-11-23 13:31:57 +0900

    Update index.js

--- wwj_web/routes/index.js
+++ wwj_web/routes/index.js
@@ -490,46 +490,32 @@ module.exports = function (app) {
               }
             } //for
 
-            var recom_list = [{list_title:'none', list_artist:'none', list_genre:'none', list_url:'none', list_mno:0}];
-            for(var t = 1; t < emotion_mno_list.length; t++){
-              var queryString3 = 'SELECT * FROM music WHERE mno=?';
-              connection.query(queryString3, emotion_mno_list[t].mno_list, function(err3, rows3){
-                if(err3){
-                  console.log(err3);
-                } else{
-                  recom_list.push({list_title:rows3[0].title, list_artist:rows3[0].artist, list_genre:rows3[0].genre, list_url:rows3[0].url, list_mno:emotion_mno_list[t].mno_list})
-                  //recom_title = rows3[0].title;
-                  //recom_artist = rows3[0].artist;
-                  //recom_genre = rows3[0].genre;
-                  //recom_url = rows3[0].url;
-                } //else
-              });//connection3
-            } //for
-
-            console.log("result start");
+            // var recom_list = [{list_title:'none', list_artist:'none', list_genre:'none', list_url:'none', list_mno:0}];
+            // for(var t = 1; t < emotion_mno_list.length; t++){
+            //   var queryString3 = 'SELECT * FROM music WHERE mno=?';
+            //   connection.query(queryString3, emotion_mno_list[t].mno_list, function(err3, rows3){
+            //     if(err3){
+            //       console.log(err3);
+            //     } else{
+            //       recom_list.push({list_title:rows3[0].title, list_artist:rows3[0].artist, list_genre:rows3[0].genre, list_url:rows3[0].url, list_mno:emotion_mno_list[t].mno_list})
+            //       recom_title = rows3[0].title;
+            //       recom_artist = rows3[0].artist;
+            //       recom_genre = rows3[0].genre;
+            //       recom_url = rows3[0].url;
+            //     } //else
+            //   });//connection3
+            // } //for
             res.render('blog-standard', {
               url: req.url,
               login: req.session.login,
               username: req.session.username,
               user_sentiment1: fst_emotion, user_sentiment2: snd_emotion,
               ant_val: ant_emotion_value, joy_val: joy_emotion_value, tru_val: tru_emotion_value, fea_val: fea_emotion_value, sur_val: sur_emotion_value, sad_val: sad_emotion_value, dis_val: dis_emotion_value, ang_val: ang_emotion_value,
-              recom_mno: recom_list[1].list_mno,
-              recom_title: recom_list[1].list_title,
-              recom_artist: recom_list[1].list_artist,
-              recom_genre: recom_list[1].list_genre,
-              recom_url: recom_list[1].list_url,
-
-              recom_mno2: recom_list[2].list_mno,
-              recom_title2: recom_list[2].list_title,
-              recom_artist2: recom_list[2].list_artist,
-              recom_genre2: recom_list[2].list_genre,
-              recom_url2: recom_list[2].list_url,
-
-              recom_mno3: recom_list[3].list_mno,
-              recom_title3: recom_list[3].list_title,
-              recom_artist3: recom_list[3].list_artist,
-              recom_genre3: recom_list[3].list_genre,
-              recom_url3: recom_list[3].list_url
+              recom_mno: m_max_mno,recom_1: m_max_emotion,
+              recom_title: recom_title,
+              recom_artist: recom_artist,
+              recom_genre: recom_genre,
+              recom_url: recom_url
             });
 
           } //else
